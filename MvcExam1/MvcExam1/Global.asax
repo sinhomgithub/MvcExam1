54c9c4dcc558f173c90358d19fcd9c24ce4de168
sinhomgithub <sinhom@edetw.com>
2016-03-21 21:11:54 +0800

    初次建立

---
+++ MvcExam1/MvcExam1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcExam1.MvcApplication" Language="C#" %>
---
+++ MvcExam1/MvcExam1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcExam1.Startup))]
+namespace MvcExam1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
